427afc2dde1ecf055e2a05a7dc1adbc25ff1b45b
JaMie <auyo18@gmail.com>
2019-05-28 10:16:42 +0800

    basic config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button} from 'antd'
+// import 'antd/dist/antd.css'
+
+const items = [
+  {
+    "id": 1,
+    "title": "去云南旅游",
+    "price": 200,
+    "data": "2019-05-28",
+    "category": {
+      "id": 1,
+      "name": "旅游",
+      "type": "outcome"
+    }
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button>提交</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
